6257982d93260c7d03e7bda32e184253e75aa57b
Anton Klock <anton@klockworks.se>
2025-01-31 10:53:32 +0100

    Removed unused import

--- components/Game.tsx
+++ components/Game.tsx
@@ -3,8 +3,8 @@ import SceneLoadingIndicators from "./SceneLoadingIndicators";
 import useGameGlobalsStore from "@/stores/gameGlobals/gameGlobals";
 import { useEffect, useState } from "react";
 import useDebugStore from "@/stores/debug/debugStore";
-import setupDebugListeners from "@/stores/debug/debugListeners";
 import DebugMenu from "./DebugMenu";
+
 export default function Game() {
   const gameGlobals = useGameGlobalsStore();
   const [initialSceneLoaded, setInitialSceneLoaded] = useState(false);

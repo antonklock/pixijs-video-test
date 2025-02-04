cc9ef4c1713812ee017f051fef9edd5c951fd422
Anton Klock <anton@klockworks.se>
2025-02-04 14:45:35 +0100

    Started using tone.js instead of regular audio element

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,31 +1,49 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import * as Tone from "tone";
 
 const musicUrl =
   "https://pxuqfsrwodxpzpensqhu.supabase.co/storage/v1/object/public/media//Ye%20Banished%20Privateers%20-%20Raise%20Your%20Glass.mp3";
 
-export default function MusicPlayer() {
-  const audioRef = useRef<HTMLAudioElement>(null);
-  const [isPlaying, setIsPlaying] = useState(true);
+const MusicPlayer = () => {
+  const playerRef = useRef<Tone.Player | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    loadMusic();
+
+    // Cleanup function
+    return () => {
+      playerRef.current?.stop();
+      playerRef.current?.dispose();
+    };
+  }, []);
+
+  const loadMusic = async () => {
+    playerRef.current = new Tone.Player(musicUrl).toDestination();
+    await playerRef.current.load(musicUrl);
+    playerRef.current.start();
+    setIsPlaying(true);
+  };
+
+  const togglePlayback = () => {
+    if (!playerRef.current) return;
+
+    if (isPlaying) {
+      playerRef.current.stop();
+      setIsPlaying(false);
+    } else {
+      playerRef.current.start();
+      setIsPlaying(true);
+    }
+  };
 
   return (
-    <>
-      <button
-        className="absolute top-10 right-10"
-        onClick={() => {
-          if (audioRef.current?.paused) {
-            audioRef.current?.play();
-            setIsPlaying(true);
-          } else {
-            audioRef.current?.pause();
-            setIsPlaying(false);
-          }
-        }}
-      >
-        {isPlaying ? "Pause" : "Play"}
-      </button>
-      <audio ref={audioRef} src={musicUrl} autoPlay />
-    </>
+    <button className="absolute top-10 right-10" onClick={togglePlayback}>
+      {isPlaying ? "Pause" : "Play"}
+    </button>
   );
-}
+};
+
+export default MusicPlayer;

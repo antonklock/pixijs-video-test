3cd2c88d5f78a98686a29d7c406cbd62f8484c13
Anton Klock <anton@klockworks.se>
2025-03-03 22:44:04 +0100

    Adding commented out countdown to be able to quickly revert if needed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 "use server";
 
 import LaunchedGame from "@/components/LaunchedGame";
+// import UnlaunchedGame from "@/components/UnlaunchedGame";
 
 export default async function DateConditionalComponent() {
+  // const targetDate = new Date("2025-03-05T16:00:00+01:00");
+  // const currentDate = new Date();
+  // const hasDatePassed = currentDate > targetDate;
+
   return <LaunchedGame />;
+
+  // if (hasDatePassed) {
+  //   return <LaunchedGame />;
+  // } else {
+  //   return <UnlaunchedGame />;
+  // }
 }

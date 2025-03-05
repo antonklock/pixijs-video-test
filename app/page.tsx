c9ddddc7bce0c799cf89c2769ea7a5c45ce524f3
Anton Klock <anton@klockworks.se>
2025-03-05 16:02:13 +0100

    Trying fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ import UnlaunchedGame from "@/components/UnlaunchedGame";
 export default async function DateConditionalComponent() {
   const targetDate = new Date("2025-03-05T16:00:00+01:00");
   const currentDate = new Date();
-  const hasDatePassed = currentDate > targetDate;
+  // const hasDatePassed = currentDate > targetDate;
+  const hasDatePassed = true;
+
+  return <LaunchedGame />;
 
   if (hasDatePassed) {
-    return <LaunchedGame />;
   } else {
     return <UnlaunchedGame />;
   }
